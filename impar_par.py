0435f0112f38d88a35974fd3ce294ed095659331
arthurgabriel3966 <arthurgabriel3966@gmail.com>
2026-04-29 22:00:35 -0300

    Create impar_par.py

---
+++ impar_par.py
@@ -0,0 +1,3 @@
+num1 = int(input("Dígite o primeiro numero: "))
+if num1 % 2 == 0: print(f"O numero {num1} é par e elevado ao quadrado é {num1**2}")
+else: print(f"O numero {num1} é impar e elevado ao cubo é {num1**3} ")
