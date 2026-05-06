5058f588af9fe5aabbd4dff548b4532e57dfbad0
arthurgabriel3966 <arthurgabriel3966@gmail.com>
2026-05-06 11:10:50 -0300

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,13 +1,24 @@
 num1 = float(input("Dígite seu 1° numero: "))
 num2 = float(input("Dígite seu 2° numero: "))
 print("Menu")
-print("""      1 - Média ponderada, com pesos 2 e 3
-      2 - Quadrado da soma dos 2 números
-      3 - Cubo do menor número""")
+print("-"*50)
+print(""" 1 - Média ponderada com peso 2 e 3
+ 2 - Quadrado da soma dos dois numeros
+ 3 - Cubo do menor numero""" )
+print("-"*50)
 opcao = input("Escolha uma opção: ")
-if opcao == "1": print(f"A média ponderada destes números é {((num1*2)+(num2*3))/(2+3)}")
-elif opcao == "2": print(f"O quadrado da soma destes 2 números é: {(num1+num2)**2}")
+if opcao == "1": 
+    resultado = ((num1*2)+(num2*3))/(2+3)
+    print(f"A média ponderada destes números é {resultado}")
+elif opcao == "2": 
+    resultado = (num1+num2)**2
+    print(f"O quadrado da soma destes 2 números é: {resultado}")
 elif opcao == "3":
-    if num1<num2: print(f"O Cubo do menor numero é {num1**3}")
-    else: print(f"O Cubo do menor numero é {num2**3}")
-else: print("Essa opção não existe")
+    if num1<num2:
+        resultado = num1**3 
+        print(f"O Cubo do menor numero é {resultado}")
+    else: 
+        resultado = num2**3
+        print(f"O Cubo do menor numero é {resultado}")
+else: 
+    print("Essa opção não existe")
