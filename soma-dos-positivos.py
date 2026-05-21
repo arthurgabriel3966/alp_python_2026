daf522cca71e7026c1fee00fbeb7e5491ac197d5
arthurgabriel3966 <arthurgabriel3966@gmail.com>
2026-05-20 22:12:25 -0300

    Create soma-dos-positivos.py

---
+++ soma-dos-positivos.py
@@ -0,0 +1,7 @@
+resultado = 0
+soma = 0
+while soma >= 0: 
+    soma = int(input(f"Dígite seu numero: "))
+    if soma >= 0:
+        resultado += soma
+print (f"A soma destes números positivos é {resultado}")
